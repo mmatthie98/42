a261eb6ff7ca0de86549251750454daf17952884
mmatthie98 <92974943+mmatthie98@users.noreply.github.com>
2022-06-29 21:29:06 +0200

    gnl is ok need to be more soft

---
+++ exam_training/ft_get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef FT_GET_NEXT_LINE_H
+#define	FT_GET_NEXT_LINE_H
+
+#define BUFFER_SIZE 42
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+int	get_next_line(char	**line);
+
+
+#endif
